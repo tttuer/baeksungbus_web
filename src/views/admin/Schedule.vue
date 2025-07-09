6ecb395c9e4bea2347287bdd90e8f89e4974d0d8
tttuer <tttuer1@naver.com>
2025-07-09 15:55:55 +0900

    아이콘 수정

--- src/views/admin/Schedule.vue
+++ src/views/admin/Schedule.vue
@@ -63,7 +63,7 @@
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5"
               />
             </svg>
           </div>
